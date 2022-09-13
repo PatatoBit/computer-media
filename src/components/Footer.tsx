dd9d5d77c5edc5e8db3a4b3bde457b3a218ba39a
PathonScript <pathonscript@gmail.com>
2022-09-13 22:04:21 +0700

    feat: mobile footer and border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,19 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <div className="w-full h-16 bg-slate-700 grid grid-cols-3 text-white">
-      <div className="flex flex-col justify-center items-center">
+    <div className="w-full h-auto bg-slate-700 grid grid-row-3 sm:grid-cols-3 text-white">
+      <div className="border-2 border-slate-800 flex flex-col justify-center items-center">
         <Link href="https://github.com/PathonScript/computer-media">
           <a target="_blank" className="text-2xl content">
-            Source Code
+            📝 Source Code
           </a>
         </Link>
+      </div>
+      <div className="border-2 border-slate-800 flex flex-col justify-center items-center">
+        <h1 className="text-2xl content">Made with ☕ by Patato, PK, Atom</h1>
+      </div>
+      <div className="border-2 border-slate-800 flex flex-col justify-center items-center">
+        <h1 className="text-2xl content">Work in Progress🛠️</h1>
       </div>
     </div>
   );
